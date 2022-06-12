259596490814e7c30dd4111d4140d491e53a7658
Stefan Dalecki <sjdalecki@gmail.com>
2022-06-12 13:16:38 -0600

    additional exception handling

--- Spotify/duplicates.py
+++ Spotify/duplicates.py
@@ -1,47 +1,93 @@
+"""Detect duplicate playlists information"""
+from __future__ import annotations
+from collections import defaultdict
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import numpy as np
-from collections import defaultdict
-from itertools import chain
 
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-playlists = sp.user_playlists(user='sdalecki')
+playlists = sp.user_playlists(user="sdalecki")
 
-months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
-          'August', 'September', 'October', 'November', 'December']
-monaten = ['Januar', 'Febuar', 'Marz', 'April', 'Mai', 'Juni', 'Juli',
-           'August', 'September', 'Oktober', 'November', 'Dezember']
+all_months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+]
+alle_monaten = [
+    "Januar",
+    "Febuar",
+    "Marz",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Dezember",
+]
+
 
 class Songs:
+    """Song data"""
+
     def __init__(self):
         self.songs = []
+        self.playlists = None
 
     def __call__(self):
+        """Detect any and all duplicates"""
         Songs.playlistgen(self)
-        print('~~~CALL BEGUN~~~', end='\n'*2)
-        print(f'{len(self.songs)} Total Songs', end='\n'*2)
+        print("CALL BEGUN", end="\n" * 2)
+        print(f"-out of {len(self.songs)} Total Songs")
         Songs.intraalbum_duplicates(self)
         Songs.interalbum_duplicates(self)
-        print('\n~~~CALL ENDED~~~', end='\n'*2)
+        print("\nCALL ENDED", end="\n" * 2)
 
     def playlistgen(self):
+        """Identify playlists"""
         self.playlists = Songs.playlistsplit(self.songs, 0)
 
-    def songs_for_emily(self, playlist, months):
-        if playlist['name'] in months:
-            tracks = sp.playlist_tracks(playlist['uri'])
-            for item in tracks['items']:
-                try:
-                    artist = item['track']['artists'][0]['name']
-                    song = item['track']['name']
-                    id = (playlist['name'], song, artist)
-                except Exception:
-                    continue
-                self.songs += [id]
+    def songs_for_emily(self, playlist: dict, months: list):
+        """All songs from Emily's playlists
 
-    def playlistsplit(exlist, ele):
+        Args:
+            playlist (dict): a single playlist
+            months (list): the playlist names (months)
+        """
+        if playlist["name"] in months:
+            tracks = sp.playlist_tracks(playlist["uri"])
+            for item in tracks["items"]:
+                try:
+                    artist = item["track"]["artists"][0]["name"]
+                    song = item["track"]["name"]
+                    song_id = (playlist["name"], song, artist)
+                except TypeError:
+                    pass
+                self.songs += [song_id]
+
+    @staticmethod
+    def playlistsplit(exlist: list, ele: str) -> list:
+        """Split playlist into components
+
+        Args:
+            exlist (list): list playlist info
+            ele (str): element of interest
+
+        Returns:
+            list: all playlist data in altered list form
+        """
         elelist = [i[ele] for i in exlist]
         cur = elelist[0]
         ind = [1, len(exlist)]
@@ -49,31 +95,32 @@ class Songs:
             if val != cur:
                 ind.append(i)
                 cur = val
-        playlists = []
+        all_playlists = []
         ind.sort()
         for a, b in zip(ind, ind[1:]):
-            playlists.append(exlist[a:b])
-        return playlists
+            all_playlists.append(exlist[a:b])
+        return all_playlists
 
     def interalbum_duplicates(self):
+        """Check for duplicate songs between albums"""
         artsong = [i[1:] for i in self.songs]
         months = [i[0] for i in self.songs]
-        dupinfo = []
         tally = defaultdict(list)
-        for i,item in enumerate(artsong):
+        for i, item in enumerate(artsong):
             tally[item].append(i)
-        dupartsong = sorted([key, locs] for key, locs in tally.items() if len(locs)>1)
+        dupartsong = sorted([key, locs] for key, locs in tally.items() if len(locs) > 1)
         for dup in dupartsong:
             dupmonths = [months[i] for i in dup[1]]
             dup[1] = dupmonths
         if dupartsong:
-            print('\ninter-album duplicates found')
+            print("\n---inter-album duplicates found")
             for i in dupartsong:
                 print(i)
         else:
-            print('\nno inter-album duplicates found')
+            print("\n---no inter-album duplicates found")
 
     def intraalbum_duplicates(self):
+        """Check for duplicate artists within an album"""
         intradups = []
         for i, playlist in enumerate(self.playlists):
             artists = [(i[2]) for i in playlist]
@@ -82,31 +129,36 @@ class Songs:
             tally = defaultdict(list)
             for i, item in enumerate(artists):
                 tally[item].append(i)
-            dupartist = sorted([key, locs] for key, locs in tally.items() if len(locs)>1)
+            dupartist = sorted(
+                [key, locs] for key, locs in tally.items() if len(locs) > 1
+            )
             if dupartist:
                 for i in dupartist[0][1]:
-                    i=songs[i]
+                    i = songs[i]
                     dupartist.append(i)
                 del dupartist[0][1]
-                dupartist[0]=dupartist[0][0]
+                dupartist[0] = dupartist[0][0]
                 dupartist.insert(0, month)
                 intradups.append(dupartist)
         if intradups:
-            print('\nintra-album duplicate(s) found')
+            print("\n---intra-album duplicate(s) found")
             for dup in intradups:
-                print(f'\n-{dup[0]}\n--{dup[1]}')
+                print(f"\n-{dup[0]}\n--{dup[1]}")
                 for song in dup[2:]:
-                    print(f'---{song}')
+                    print(f"---{song}")
 
         else:
-            print('\nno intra-album duplicates found')
+            print("\n---no intra-album duplicates found")
+
 
 music = Songs()
+print("\n---Identifying Playlists---\n")
 while playlists:
-    for i, playlist in enumerate(playlists['items']):
-        tracks = music.songs_for_emily(playlist, months+monaten)
-    if playlists['next']:
+    for index, emilys_playlist in enumerate(playlists["items"]):
+        music.songs_for_emily(emilys_playlist, all_months + alle_monaten)
+    if playlists["next"]:
         playlists = sp.next(playlists)
     else:
         playlists = None
+print("---Playlists Identified---", end="\n" * 3)
 music()
